064f6ddec9b5dd07f7de3794bab67c73c4966ee0
Florian Diesel <florian.diesel@posteo.de>
2023-03-15 22:53:22 +0100

    initial commit

---
+++ dist/index.js
@@ -0,0 +1,12 @@
+import * as core from "@actions/core";
+import { readFileSync } from "fs";
+function getPackageVersion() {
+    const filePath = core.getInput("path");
+    const fileRaw = readFileSync(filePath, { encoding: "utf8" });
+    const file = JSON.parse(fileRaw);
+    return file.version;
+}
+async function main() {
+    console.log(getPackageVersion());
+}
+main();
