686a0dd05fad572b2455d61a993d895f08dc71ec
Florian Diesel <florian.diesel@posteo.de>
2023-03-19 21:10:06 +0100

    feat: output changelog

--- src/index.ts
+++ src/index.ts
@@ -82,10 +82,11 @@ async function run() {
 
   const exists = await releaseExists(octokit, prefixedPackageVersion);
 
+  let releaseNotes;
+
   if (exists) {
     core.notice(`Release and Tag '${prefixedPackageVersion}' already exists`);
   } else {
-    let releaseNotes;
     if (generateReleaseNotes) {
       const commitHash = await latestReleaseCommitHash(octokit);
       const commitMessages = await commitMessagesSinceCommitHash(octokit, commitHash);
@@ -96,6 +97,7 @@ async function run() {
 
   core.setOutput("created", created);
   core.setOutput("version", packageVersion);
+  core.setOutput("changelog", releaseNotes || "");
 }
 
 run();
