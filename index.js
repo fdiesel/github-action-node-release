0da7f4e44a009e954d8c2c85afd169bff8f49435
fdiesel <72613036+fdiesel@users.noreply.github.com>
2024-01-05 12:43:33 +0000

    Update dist to output generated at d7fb202

--- index.js
+++ index.js
@@ -4,94 +4,94 @@ import { readFileSync } from 'fs';
 let created = false;
 const { repo, owner } = github.context.repo;
 function getPackageVersion() {
-  const filePath = core.getInput('path');
-  const fileRaw = readFileSync(filePath, { encoding: 'utf8' });
-  const file = JSON.parse(fileRaw);
-  return file.version;
+    const filePath = core.getInput('path');
+    const fileRaw = readFileSync(filePath, { encoding: 'utf8' });
+    const file = JSON.parse(fileRaw);
+    return file.version;
 }
 async function releaseExists(octokit, tagName) {
-  try {
-    await octokit.rest.repos.getReleaseByTag({
-      owner,
-      repo,
-      tag: tagName
-    });
-    return true;
-  } catch {
-    return false;
-  }
+    try {
+        await octokit.rest.repos.getReleaseByTag({
+            owner,
+            repo,
+            tag: tagName
+        });
+        return true;
+    }
+    catch {
+        return false;
+    }
 }
 async function createRelease(octokit, tagName, releaseNotes = '') {
-  try {
-    const res = await octokit.rest.repos.createRelease({
-      owner,
-      repo,
-      tag_name: tagName,
-      name: tagName,
-      target_commitish: github.context.sha,
-      body: releaseNotes
-    });
-    core.notice(`Created tag and release '${tagName}'`);
-    created = true;
-    return res.data;
-  } catch (error) {
-    core.setFailed(`Action failed with error ${error}`);
-  }
+    try {
+        const res = await octokit.rest.repos.createRelease({
+            owner,
+            repo,
+            tag_name: tagName,
+            name: tagName,
+            target_commitish: github.context.sha,
+            body: releaseNotes
+        });
+        core.notice(`Created tag and release '${tagName}'`);
+        created = true;
+        return res.data;
+    }
+    catch (error) {
+        core.setFailed(`Action failed with error ${error}`);
+    }
 }
 async function latestReleaseDate(octokit) {
-  try {
-    return (
-      await octokit.rest.repos.getLatestRelease({
-        owner,
-        repo
-      })
-    ).data.published_at;
-  } catch (error) {
-    return undefined;
-  }
+    try {
+        return (await octokit.rest.repos.getLatestRelease({
+            owner,
+            repo
+        })).data.published_at;
+    }
+    catch (error) {
+        return undefined;
+    }
 }
 async function commitMessagesSinceCommitHash(octokit, since) {
-  try {
-    return (
-      await octokit.rest.repos.listCommits({
-        owner,
-        repo,
-        since: since
-      })
-    ).data.map((commit) => commit.commit.message);
-  } catch (error) {
-    core.setFailed(`Action failed with error ${error}`);
-  }
+    try {
+        return (await octokit.rest.repos.listCommits({
+            owner,
+            repo,
+            since: since
+        })).data.map((commit) => commit.commit.message);
+    }
+    catch (error) {
+        core.setFailed(`Action failed with error ${error}`);
+    }
 }
 async function run() {
-  const dry_run = core.getInput('dry_run') == 'true';
-  const packageVersion = getPackageVersion();
-  const prefixedPackageVersion = core.getInput('prefix') + packageVersion;
-  const token = core.getInput('token');
-  const octokit = github.getOctokit(token);
-  const generateReleaseNotes = core.getInput('notes') == 'true';
-  const exists = await releaseExists(octokit, prefixedPackageVersion);
-  let releaseNotes;
-  let version_changed = true;
-  if (exists) {
-    core.notice(`Release and Tag '${prefixedPackageVersion}' already exists`);
-    version_changed = false;
-  } else if (!dry_run) {
-    if (generateReleaseNotes) {
-      const date = await latestReleaseDate(octokit);
-      core.info(`Latest release commit hash: ${date}`);
-      const commitMessages = await commitMessagesSinceCommitHash(
-        octokit,
-        date ? date : undefined
-      );
-      commitMessages?.forEach((msg) => core.info(msg));
-      releaseNotes = commitMessages?.join('\n');
+    const dryRun = core.getInput('dry_run') == 'true';
+    const packageVersion = getPackageVersion();
+    const prefixedPackageVersion = core.getInput('prefix') + packageVersion;
+    const token = core.getInput('token');
+    const octokit = github.getOctokit(token);
+    const generateReleaseNotes = core.getInput('notes') == 'true';
+    const exists = await releaseExists(octokit, prefixedPackageVersion);
+    let releaseNotes;
+    let versionHasChanged = true;
+    if (exists) {
+        core.notice(`Release and Tag '${prefixedPackageVersion}' already exists`);
+        versionHasChanged = false;
     }
-    await createRelease(octokit, prefixedPackageVersion, releaseNotes);
-  }
-  core.setOutput('created', created);
-  core.setOutput('version', packageVersion);
-  core.setOutput('changelog', releaseNotes || '');
-  core.setOutput('version_changed', version_changed);
+    else {
+        if (generateReleaseNotes) {
+            const date = await latestReleaseDate(octokit);
+            core.info(`Latest release commit hash: ${date}`);
+            const commitMessages = await commitMessagesSinceCommitHash(octokit, date ? date : undefined);
+            commitMessages?.forEach((msg) => core.info(msg));
+            releaseNotes = commitMessages?.join('\n');
+        }
+        if (!dryRun) {
+            await createRelease(octokit, prefixedPackageVersion, releaseNotes);
+        }
+    }
+    core.setOutput('created', created);
+    core.setOutput('version', packageVersion);
+    core.setOutput('changelog', releaseNotes || '');
+    core.setOutput('version_changed', versionHasChanged);
 }
 run();
